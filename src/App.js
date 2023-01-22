173e2d19317c867b409ab28667d409226b13b81a
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2023-01-22 22:58:58 +0100

    Imported the Navbar component inside the App component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Project from './components/projects/Projects';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
       <About />
       <Skills />
